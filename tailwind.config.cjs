e433b81997d7d22548abd39447ffa5f1e3e5f844
KiranKrishna004 <kiran.004narath@gmail.com>
2022-10-01 23:17:56 +0530

    feat: add font and xs breakpoint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		screens: {
+			xs: "300px",
+			...defaultTheme.screens,
+		},
+		fontFamily: { sans: ["inter"] },
 	},
 	plugins: [],
 }
