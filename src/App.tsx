6b04e560ffa32f9dfdc3afa632049cf4bb386cdf
KiranKrishna004 <kiran.004narath@gmail.com>
2022-10-02 16:05:29 +0530

    refactor: move indicator to a component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css"
 import { Phases } from "./components/Phases"
 import edenLogo from "./assets/edenLogo.png"
 import { useState } from "react"
+import { Indicator } from "./components/Indicator"
 const App = () => {
 	const [curPhase, setCurPhase] = useState(1)
 	return (
@@ -10,42 +11,7 @@ const App = () => {
 				<img className='h-10' src={edenLogo} alt='logo missing' />
 				<p className='text-3xl font-semibold'>Eden</p>
 			</div>
-			<div className='flex'>
-				{[...Array(4)].map((item, index) => {
-					return (
-						<>
-							<p
-								key={index + 1}
-								className={
-									index + 1 <= curPhase
-										? "bg-[#664DE5] my-20 text-white text-sm px-4 py-2.5 rounded-full"
-										: "border text-black my-20 text-sm px-4 py-2.5 rounded-full"
-								}
-							>
-								{index + 1}
-							</p>
-							{index + 1 !== 4 && (
-								<>
-									<div
-										className={
-											index + 1 <= curPhase
-												? "bg-[#664DE5] h-[0.1px] w-7 self-center"
-												: "bg-gray-300 h-[0.1px] w-7 self-center"
-										}
-									></div>
-									<div
-										className={
-											index + 1 < curPhase
-												? "bg-[#664DE5] h-[0.1px] w-7 self-center"
-												: "bg-gray-300 h-[0.1px] w-7 self-center"
-										}
-									></div>
-								</>
-							)}
-						</>
-					)
-				})}
-			</div>
+			<Indicator curPhase={curPhase} />
 			<Phases curPhase={curPhase} setCurPhase={setCurPhase} />
 		</div>
 	)
---
+++ src/components/Indicator.tsx
@@ -0,0 +1,40 @@
+export const Indicator = ({ curPhase }: { curPhase: number }) => {
+	return (
+		<div className='flex'>
+			{[...Array(4)].map((item, index) => {
+				return (
+					<>
+						<p
+							key={index + 1}
+							className={
+								index + 1 <= curPhase
+									? "bg-[#664DE5] my-20 text-white text-sm px-4 py-2.5 rounded-full"
+									: "border text-black my-20 text-sm px-4 py-2.5 rounded-full"
+							}
+						>
+							{index + 1}
+						</p>
+						{index + 1 !== 4 && (
+							<>
+								<div
+									className={
+										index + 1 <= curPhase
+											? "bg-[#664DE5] h-[0.1px] w-7 self-center"
+											: "bg-gray-300 h-[0.1px] w-7 self-center"
+									}
+								></div>
+								<div
+									className={
+										index + 1 < curPhase
+											? "bg-[#664DE5] h-[0.1px] w-7 self-center"
+											: "bg-gray-300 h-[0.1px] w-7 self-center"
+									}
+								></div>
+							</>
+						)}
+					</>
+				)
+			})}
+		</div>
+	)
+}
